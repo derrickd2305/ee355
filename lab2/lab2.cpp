322e2f2d2422f309b446e3e1553816c0b17388d4
derrickd2305 <derrickd@usc.edu>
2025-01-29 17:51:31 -0800

    Update lab2.cpp

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -9,10 +9,11 @@ class Matrix {
     int target_row_idx = 0;
     int maximum = 0;
     int minimum = 0;
-
+/*
     void linear_search();
     void mat_add(mat1, mat2);
     void mat_mult(mat1, mat2);
+*/
 }
 
 // https://www.w3schools.com/cpp/ref_fstream_ifstream.asp
