93c3706c066d0bea81ecdbdbee57e7f441697cd7
derrickd2305 <derrickd@usc.edu>
2025-01-29 22:33:53 -0800

    fix

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <ifstream>
+#include <fstream>
 #include <sstream>
 #include <string>
 using namespace std;
 
 class Matrix {
     int data[10][10]; 
-    int target_row_idx = 0;
-    int maximum = 0;
-    int minimum = 0;
+    int target_row_idx;
+    int maximum;
+    int minimum;
 /*
     void linear_search();
     void mat_add(mat1, mat2);
     void mat_mult(mat1, mat2);
 */
-}
+};
 
 // https://www.w3schools.com/cpp/ref_fstream_ifstream.asp
 
@@ -26,14 +26,14 @@ int main(){
     Matrix matrix2; 
 
     for(int i = 0; i < 10; i++){
-        for(int j = 0; j < size; j++){
-            file1 >> matrix1[i][j];
-            file2 >> matrix2[i][j]; 
+        for(int j = 0; j < 10; j++){
+            file1 >> matrix1.data[i][j];
+            file2 >> matrix2.data[i][j]; 
         }
     }
 
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
             cout << matrix[i][j] << "\t"; // Print each element
         }
         cout << "\n"; // New line for each row
