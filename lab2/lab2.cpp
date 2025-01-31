52bd92007a76628d48d710cfaf947c3912b8a2b6
derrickd2305 <derrickd@usc.edu>
2025-01-31 11:10:33 -0800

    typo

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -39,7 +39,7 @@ public:
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
                 for(int k = 0; k < 10; k++){
-                    product_mat.data[i][j] = product_mat.data[i][j] + data[i][k] + mat2.data[k][j]
+                    product_mat.data[i][j] = product_mat.data[i][j] + data[i][k] + mat2.data[k][j];
                 }
             }
         }
