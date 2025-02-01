de76cb9400e503c5c563473fdce725228413aedd
derrickd2305 <derrickd@usc.edu>
2025-01-31 17:23:49 -0800

    implemented linear search

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -7,9 +7,11 @@ using namespace std;
 class Matrix {
 public:
     int data[10][10]; 
-    int target_row_idx;
+    int target_row = -1;
     int maximum;
+    int max_row;
     int minimum;
+    int min_row;
 
     void print_mat(){
         for (int i = 0; i < 10; i++) {
@@ -42,6 +44,30 @@ public:
         }
         return product_mat;
     }
+
+    void linear_search(int &max, int &min, int &max_row, int &min_row, int &target_row){
+        int first_num = data[0][0];
+        max = data[0][0];
+        min = data[0][0];
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                if(max < data[i][j]){
+                    max = data[i][j];
+                    max_row = i;
+                }
+                if(min > data[i][j]){
+                    min = data[i][j];
+                    min_row = i;
+                }
+                if(i == 0 && j == 0){
+                    continue;
+                }
+                if(first_num == data[i][j]){
+                    target_row = i; 
+                }
+            }
+        }
+    }
 };
 
 // referred to this website on how to use fstream to pull info from text files https://www.w3schools.com/cpp/ref_fstream_ifstream.asp
@@ -65,6 +91,9 @@ int main(){
     sum_mat1_mat2 = matrix1.mat_add(matrix2);
     mult_mat1_mat2 = matrix1.mat_mult(matrix2);
 
+    matrix1.linear_search(maximum, minimum, max_row, min_row, target_row);
+    matrix2.linear_search(maximum, minimum, max_row, min_row, target_row);
+
     cout<<"matrix1: \n";
     matrix1.print_mat();
     cout<<"\n";
@@ -75,6 +104,15 @@ int main(){
     sum_mat1_mat2.print_mat();
     cout<<"\n product of matrix 1 and 2: \n";
     mult_mat1_mat2.print_mat();
+
+    cout << "matrix1: max: " << matrix1.maximum << " at row " << matrix1.max_row << endl;
+    cout << "matrix2: min: " << matrix2.maximum << " at row " << matrix2.max_row << endl;
+    if(matrix1.target_row == -1){
+        cout << "first value is not repeated";
+    }
+    else{
+        cout << "first value reappears at row: " << target_row << endl; 
+    }
 
 
     return 0;
