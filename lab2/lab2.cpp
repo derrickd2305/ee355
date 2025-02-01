1acfd823040ce1779ca80085f3f5edc1b55f6bed
derrickd2305 <derrickd@usc.edu>
2025-01-31 17:35:27 -0800

    Update lab2.cpp

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -53,17 +53,17 @@ public:
             for(int j = 0; j < 10; j++){
                 if(max < data[i][j]){
                     max = data[i][j];
-                    max_row = i;
+                    max_row = i + 1;
                 }
                 if(min > data[i][j]){
                     min = data[i][j];
-                    min_row = i;
+                    min_row = i + 1;
                 }
                 if(i == 0 && j == 0){
                     continue;
                 }
                 if(first_num == data[i][j]){
-                    target_row = i; 
+                    target_row = i + 1; 
                 }
             }
         }
@@ -100,7 +100,17 @@ int main(){
     cout<<"matrix1: \n";
     matrix1.print_mat();
     cout<<"\n";
-    cout<<"matrix2: \n";
+    
+    cout << "matrix1: max: " << matrix1.max << " at row " << matrix1.max_row << endl;
+    cout << "matrix1: min: " << matrix1.min << " at row " << matrix1.min_row << endl;
+    if(matrix1.target_row == -1){
+        cout << "first value is not repeated";
+    }
+    else{
+        cout << "first value reappears at row: " << matrix1.target_row << endl; 
+    }
+    
+    cout<<" \n matrix2: \n";
     matrix2.print_mat();
     cout<<"\n";
     cout<<"sum of matrix 1 and 2: \n";
@@ -108,14 +118,7 @@ int main(){
     cout<<"\n product of matrix 1 and 2: \n";
     mult_mat1_mat2.print_mat();
 
-    cout << "matrix1: max: " << matrix1.max << " at row " << matrix1.max_row << endl;
-    cout << "matrix2: min: " << matrix2.min << " at row " << matrix2.min_row << endl;
-    if(matrix1.target_row == -1){
-        cout << "first value is not repeated";
-    }
-    else{
-        cout << "first value reappears at row: " << matrix1.target_row << endl; 
-    }
+    
 
 
     return 0;
