a8f65dd2d68a7a5f6d614c872a8f0d20436cbbdc
derrickd2305 <derrickd@usc.edu>
2025-02-12 15:01:27 -0800

    m

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+class Person {
+    public:
+        void displayPersonInfo(){
+            cout<<"name: "<<name<<endl<<"age: "<<age<<endl;
+        }
+    protected:
+        string name = "derrick";
+        int age = 19;
+};
+
+class Student : public Person {
+
+};
+
+class StudentPrivate : private Person {
+
+};
+
+int main(){
+    Student student1;
+    StudentPrivate student2;
+    student1.displayPersonInfo();
+    student2.displayPersonInfo();
+}
