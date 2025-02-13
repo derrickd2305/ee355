c6d802f742bf0b86588b91cf0ddfef9100b359a9
derrickd2305 <derrickd@usc.edu>
2025-02-12 17:30:04 -0800

    hello

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -6,12 +6,15 @@ using namespace std;
 
 class Person {
     public:
+        void Person(string newName, int newAge){
+            this->name = newName;
+            this->age = newAge;
         void displayPersonInfo(){
             cout<<"name: "<<name<<endl<<"age: "<<age<<endl;
         }
     protected:
-        string name = "derrick";
-        int age = 19;
+        string name;
+        int age;
 };
 
 class Student : public Person {
@@ -23,8 +26,7 @@ class StudentPrivate : private Person {
 };
 
 int main(){
-    Student student1;
-    StudentPrivate student2;
+    Student student1("derrick", 19);
     student1.displayPersonInfo();
-    student2.displayPersonInfo();
+    //student2.displayPersonInfo();
 }
