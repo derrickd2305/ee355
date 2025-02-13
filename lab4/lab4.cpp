cb53aa7b627bbd42f3b0bc282163ef15f7a88b67
derrickd2305 <derrickd@usc.edu>
2025-02-12 19:19:04 -0800

    changed outputs

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -20,25 +20,52 @@ class Person {
 
 class Student : public Person {
     public:
+        Student(string newName, int newAge, int newID, int newGPA){
+            name = newName;
+            age = newAge;
+            ID = newID;
+            GPA = newGPA;
+        }
+        int ID;
+        int GPA;
         void displayPersonInfo(){
-            cout<<"Student: "<<name<<" Age: "<<age<<endl;
+            cout<<"Student: "<<name<<" "<<age<<" "<<ID<<" "<<GPA<<endl;
         }
 };
 
 class StudentPrivate : private Person {
-
+    public:
+        StudentPrivate(string newName, int newAge, int newID, float newGPA){
+            name = newName;
+            age = newAge;
+            ID = newID;
+            GPA = newGPA;
+        }
+        int ID;
+        float GPA;
+        void displayPersonInfo(){
+            cout<<"Private student: "<<name<<" "<<age<<" "<<ID<<" "<<GPA<<endl;
+        }
 };
 
 class Teacher : public Person {
     public:
+        Teacher(string newName, int newAge, string newSubject, int newYearsOfExp){
+            name = newName;
+            age = newAge;
+            subject = newSubject;
+            yearsOfExp = newYearsOfExp;
+        }
+        string subject;
+        int yearsOfExp;
         void displayPersonInfo(){
-            cout<<"Teacher: "<<name<<" Age: "<<age<<endl;
+            cout<<"Teacher: "<<name<<" "<<age<<" "<<subject<<" "<<yearsOfExp<<endl;
         }
 };
 
 int main(){
     Student student1;
-    student1.setPersonInfo("derrick", 19);
+    student1("Derrick", 19, 677, 4.0);
     student1.displayPersonInfo();
     
     //StudentPrivate student2;
@@ -46,6 +73,6 @@ int main(){
     //student2.displayPersonInfo();
     
     Teacher teacher1;
-    teacher1.setPersonInfo("Mr. Kim", 91);
-    teacher1.displayPersonInfo();
+    teacher1("Bob", 45, "EE355", 15);
+    teacher1.displayPersonInfo();   
 }
