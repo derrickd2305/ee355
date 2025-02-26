489adfe72c3027c6120a5b0f00565d84d9398efa
derrickd2305 <derrickd@usc.edu>
2025-02-26 15:28:30 -0800

    m

--- lab6/fibonacci.py
+++ lab6/fibonacci.py
@@ -4,9 +4,11 @@ import sys
 # using sys to pass the input from command line
 n = sys.argv[1]
 
+# using subprocess to run the the c++""
 subprocess.run(["g++", "fibonacci_dp.cpp", "-o", "./fibonacci_dp.out"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 result = subprocess.run(["./fibonacci_dp.out", str(n)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+# store the output in an array
 output = result.stdout.decode().strip().splitlines()
 
 if output[0] == "1":
