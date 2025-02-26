06e9776fb11d3fe2a653555e370d5e65da87329b
derrickd2305 <derrickd@usc.edu>
2025-02-26 13:07:42 -0800

    m

--- lab6/fibonacci_dp.cpp
+++ lab6/fibonacci_dp.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <cstdlib> // for atoi
+using namespace std;
+
+int fibonacci_dp(int n, int mem[]){
+    // base case
+    if(n <= 1){
+        return n;
+    }
+
+    // check if we haven't computed that fibonacci number already
+    if(mem[n] == -1){
+        mem[n] = fibonacci_dp(n-1, mem) + fibonacci_dp(n-2, mem);
+    }
+    return mem[n];
+}
+
+int main(int argc, char* argv[]){
+    // obtain input from command line
+    int input = atoi(argv[1]);
+
+    // allocate memory for fibonacci number, what term it is, and an array for memoization
+    int fibonacci = 1, n = 1;
+    int* mem = new int[input];
+
+    // fill array with initial value as a way to check if we have computed that value before
+    for(int i = 0; i < input; i++){
+        mem[i] = -1;
+    }
+
+    // keep calculating fibonacci numbers bottom-up, until our fibonacci number is equal to or greater than the input
+    while(fibonacci < input){
+        fibonacci = fibonacci_dp(++n);
+    }
+    if(fibonacci == input){
+        cout << "1" << endl;
+    }
+    else{
+        cout << "0" << endl << fibonacci;
+    }
+    delete [] mem;
+    return 0;
+}
 
--- lab6/fibonacci_recursive.cpp
+++ lab6/fibonacci_recursive.cpp
@@ -3,15 +3,22 @@
 using namespace std;
 
 int fibonacci_recursive(int n){
+    // base case
     if(n <= 1){
         return n;
     }
+    // compute the fibonacci number recursively from scratch for every term other than the base case
     return fibonacci_recursive(n-1) + fibonacci_recursive(n-2);
 }
 
 int main(int argc, char* argv[]){
+    // getting output from command line, converting to integer
     int input = atoi(argv[1]);
+
+    // allocated memory for fibonacci number and counter
     int fibonacci = 1, n = 1;
+
+    // keep calculating fibonacci numbers bottom-up until it is equal to or greater than our input
     while(fibonacci < input){
         fibonacci = fibonacci_recursive(++n);
     }
