1a35c1591fe13005d49cfdf9a286e558890d1399
derrickd2305 <derrickd@usc.edu>
2025-02-26 15:08:51 -0800

    m

--- lab6/fibonacci.py
+++ lab6/fibonacci.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("n", type = int)
 args = parser.parse_args()
 
-subprocess.run(["g++", "fibonacci_dp.cpp", "-o", "./fibonacci_dp.out"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text = True)
-result = subprocess.run(["./fibonacci_dp.out", str(args.n)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text = True)
+subprocess.run(["g++", "fibonacci_dp.cpp", "-o", "./fibonacci_dp.out"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+result = subprocess.run(["./fibonacci_dp.out", str(args.n)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 print(result.stdout)
