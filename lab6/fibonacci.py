85594684a5dad74cb08b78ea57ecfc1b56c2ef19
derrickd2305 <derrickd@usc.edu>
2025-02-26 15:02:03 -0800

    m

--- lab6/fibonacci.py
+++ lab6/fibonacci.py
@@ -3,10 +3,11 @@ import argparse
 
 # using argparse to pass the input from command line
 parser = argparse.ArgumentParser()
-parser.add_argument("n", type = int)
+parser.add_argument("n", type = string)
 args = parser.parse_args()
 
 subprocess.run(["g++", "fibonacci_dp.cpp", "-o", "./fibonacci_dp.out"])
 result = subprocess.run(["./fibonacci_dp.out", args.n], capture_output = True, text = True)
 
-output_lines = result.stdout
+output = result.stdout
+print(output)
