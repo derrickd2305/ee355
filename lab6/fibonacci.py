b566c08544c5123f7f23505322a5f4a0e44d11d4
derrickd2305 <derrickd@usc.edu>
2025-02-26 15:21:23 -0800

    m

--- lab6/fibonacci.py
+++ lab6/fibonacci.py
@@ -8,5 +8,10 @@ subprocess.run(["g++", "fibonacci_dp.cpp", "-o", "./fibonacci_dp.out"], stdout=s
 result = subprocess.run(["./fibonacci_dp.out", str(input)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 output = result.stdout.decode().strip().splitlines()
-for n in output:
-    print(n)
+
+if output[0] == 1:
+    print(f"Congratulations! You have the Fibonacci {input}")
+elif output[0] == 0:
+    print(f"Sorry! {input} is not a Fibonacci number but the next Fibonacci number to it is {output[1]}")
+else:
+    print("something went wrong with output of cpp function")
