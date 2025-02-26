dd1d344dd01272d37b70a6e68bf1c688afd3f65a
derrickd2305 <derrickd@usc.edu>
2025-02-26 15:22:59 -0800

    m

--- lab6/fibonacci.py
+++ lab6/fibonacci.py
@@ -2,16 +2,16 @@ import subprocess
 import sys
 
 # using sys to pass the input from command line
-input = sys.argv[1]
+n = sys.argv[1]
 
 subprocess.run(["g++", "fibonacci_dp.cpp", "-o", "./fibonacci_dp.out"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-result = subprocess.run(["./fibonacci_dp.out", str(input)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+result = subprocess.run(["./fibonacci_dp.out", str(n)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 output = result.stdout.decode().strip().splitlines()
 
-if output[0] == 1:
-    print(f"Congratulations! You have the Fibonacci {input}")
-elif output[0] == 0:
+if output[0] == "1":
+    print(f"Congratulations! You have the Fibonacci {n}")
+elif output[0] == "0":
     print(f"Sorry! {input} is not a Fibonacci number but the next Fibonacci number to it is {output[1]}")
 else:
     print("something went wrong with output of cpp function")
