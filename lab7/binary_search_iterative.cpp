be341a8366005f26b054950a01f2b51699818e2d
derrickd2305 <derrickd@usc.edu>
2025-03-05 23:22:43 -0800

    m

--- lab7/binary_search_iterative.cpp
+++ lab7/binary_search_iterative.cpp
@@ -1,1 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
 
+using namespace std;
+
+int main(){
+    // starter code **********************
+    ifstream inputFile("input.txt");
+    if(!inputFile){
+        cout<<"unable to open file"<<endl;
+        return 1;
+    }
+
+    int n;
+    inputFile >> n;
+
+    int* arr = new int[n];
+    for(int i = 0; i < n; i++){
+        inputFile >> arr[i];
+    }
+
+    int target;
+    inputFile >> target; 
+
+    inputFile.close();
+
+    sort(arr, arr + n);
+    // *************************************
+
+    // declaring variables to partition and index through array
+    int start = 0, end = n - 1;
+    int half = (start + end)/2;
+
+    while(target != arr[half]){
+        // target not in array
+        if(start > end){
+            cout << "Not Found";
+            return 0;
+        }
+        // check upper half
+        else if(target > arr[half]){
+            start = half + 1;
+        }
+        // check lower half
+        else if(target < arr[half]){
+            end = half - 1;
+        }
+        half = (start + end)/2;
+    }
+
+    // print statement will only execute if target was found
+    cout << half << endl;
+
+    delete [] arr;
+
+    return 0;
+}
--- lab7/binary_search_recursive.cpp
+++ lab7/binary_search_recursive.cpp
@@ -4,25 +4,32 @@
 
 using namespace std;
 
+// implementing binary search recursively
 void binary_search_recursive(int target, int* arr, int start, int end){
+    // target is not in array
     if(start > end){
         cout << "No Found" << endl;
         return;
     }
     int half = (start + end)/2;
+    // target is found
     if(target == arr[half]){
         cout << half << endl;
         return;
     }
+    // check lower half
     else if(target < arr[half]){
         binary_search_recursive(target, arr, start, half - 1);
     }
+    // check upper half
     else{
         binary_search_recursive(target, arr, half + 1, end);
     }
 }
 
 int main(){
+
+    // starter code ************************
     ifstream inputFile("input.txt");
     if(!inputFile){
         cout<<"unable to open file"<<endl;
@@ -43,8 +50,11 @@ int main(){
     inputFile.close();
 
     sort(arr, arr + n);
+    // ****************************************
 
     binary_search_recursive(target, arr, 0, n-1);
 
     delete [] arr;
+
+    return 0;
 }
