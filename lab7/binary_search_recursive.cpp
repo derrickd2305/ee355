9c4822bf10908a2ab2e87678d676ce8d2f049bff
derrickd2305 <derrickd@usc.edu>
2025-03-05 21:02:32 -0800

    m

--- lab7/binary_search_recursive.cpp
+++ lab7/binary_search_recursive.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
 
+void binary_search_recursive(int target, int* arr, int start, int end){
+    if(start == end || start > end){
+        cout << "No Found";
+    }
+    int half = (start + end)/2;
+    if(target == arr[half]){
+        cout << half;
+    }
+    else if(target < arr[half]){
+        binary_search_recursive(target, arr, start, half);
+    }
+    else{
+        binary_search_recursive(target, arr, half, end);
+    }
+}
+
+int main(){
+    ifstream inputFile("input.txt");
+    if(!inputFile){
+        cout<<"unable to open file"<<endl;
+        return 1;
+    }
+
+    int n;
+    inputFile >> n;
+
+    int* arr = new int[n];
+    for(int i = 0; i < n; i++){
+        inputFile >> arr[i];
+    }
+
+    int target;
+    inputFile >> target; 
+
+    inputFile.close();
+
+    sort(arr, arr + n);
+
+    binary_search_recursive(target, arr, 0, n-1);
+}
