c05e1be3458a13e6d8826017ee0a53aed45dbb45
derrickd2305 <derrickd@usc.edu>
2025-03-06 21:32:15 -0800

    m

--- lab7/quick_sort.cpp
+++ lab7/quick_sort.cpp
@@ -1,1 +1,81 @@
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+void quickSort(int* arr, n){
+    // base case: array is size 1 or less
+    if(n <= 1){
+        return;
+    }
+    
+    // aribitrarily assinging pivot as first 
+    int pivot = arr[0];
+    // allocating memory for three arrays
+    int c1 = 0, c2 = 0, c3 = 0;
+    int* equal = new int[n];
+    int* less = new int[n-1];
+    int* greater = new int[n-1];
+
+    // copying elements into different arrays depending on if they are less than, greater than, or equal to the pivot value
+    for(int i = 0; i < n; i++){
+        if(arr[i] == pivot){
+            equal[c1] = arr[i];
+            c1++;
+        }
+        else if(arr[i] < pivot){
+            less[c2] = arr[i];
+            c2++;
+        }
+        else{
+            greater[c3] = arr[i];
+            c3++;
+        }
+    }
+
+    // recursive calls to split and sort
+    quickSort(equal, c1);
+    quickSort(less, c2);
+    quickSort(greater, c3);
+
+    // replacing original array with sorted arrays
+    for(int i = 0; i < c2; i++){
+        arr[i] = less[i];
+    }
+    for(int i = 0; i < c1; i++){
+        arr[i + c2] = equal[i];
+    }
+    for(int i = 0; i < c3; i++){
+        arr[i + c1 + c2] = greater[i];
+    }
+
+}
+
+int main(){
+    // starter code ***************************
+    ifstream inputFile("input.txt");
+    if(!inputFile){
+        cout<<"unable to open file"<<endl;
+        return 1;
+    }
+
+    int n;
+    inputFile >> n;
+
+    int* arr = new int[n];
+    for(int i = 0; i < n; i++){
+        inputFile >> arr[i];
+    }
+    inputFile.close();
+    // ***************************************
+
+    quickSort(arr, n);
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    outputFile.close();
+    delete [] arr;
+    return 0;
+}
