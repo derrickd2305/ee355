08345eeccacff1dbe7fe67bdf1fcb065d6a08c35
derrickd2305 <derrickd@usc.edu>
2025-03-10 14:39:09 -0700

    m

--- hw/hw5/part2-1.cpp
+++ hw/hw5/part2-1.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
+using namespace std;
+
+ofstream outFile("output_ToH.txt");
+int moveCounter = 0; // Counter for the number of moves
+
+void moveDisk(int n, char from_rod, char to_rod) {
+    outFile << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
+    moveCounter++; // Increment the counter
+}
+
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
+    // base case: one disk
+    if(n <= 1){
+        moveDisk(1, from_rod, to_rod);
+        return;
+    }
+
+    // move all disks except bottom one to the aux rod, so the og to_rod becomes the aux_rod
+    towerOfHanoi(n/3, from_rod, aux_rod, to_rod);
+    // move the bottom disk
+    moveDisk(n/3, from_rod, to_rod);
+    // then clear the aux_rod so we can move the bottom one into place, then repeat
+    towerOfHanoi(n/3, aux_rod, to_rod, from_rod);
+    
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " <number_of_disks>" << endl;
+        return 1;
+    }
+
+    int n = atoi(argv[1]); // Number of disks
+    towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
+    outFile << "Total number of moves: " << moveCounter << endl; // Output the total number of moves
+    outFile.close();
+    return 0;
+}
