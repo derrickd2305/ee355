1a5f8456c82b10d23eb63361e73acbe2a06d89ac
derrickd2305 <derrickd@usc.edu>
2025-03-10 14:29:51 -0700

    m

--- hw/hw5/part1-3.cpp
+++ hw/hw5/part1-3.cpp
@@ -4,5 +4,26 @@
 
 using namespace std;
 
+fibonacci(int n){
+    if(n <= 1){
+        return n;
+    }
+    return fibonacci_recursive(n-1) + fibonacci_recursive(n-2);
+}
+
 int main(){
+    int n;
+    cout << "generate a Fibonacci series with n terms: ";
+    cin >> n;
+
+    int* arr = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = fibonacci(i);
+    }
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete [] arr;
 }
