73e2a903b45aa732c02ad91ff3b6ec7aba3a2724
derrickd2305 <derrickd@usc.edu>
2025-03-10 14:17:12 -0700

    m

--- hw/hw5/part1-2.cpp
+++ hw/hw5/part1-2.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+using namespace std;
+
+int factorial(int val){
+    if(val <= 1){
+        return 1;
+    }
+    return val * factorial(val-1);
+}
+
+int main(){
+    int n; 
+    cout << "find the factorial of: ";
+    cin >> n;
+
+    int fact = factorial(n);
+
+    cout << "factorial of " << n << ": " << fact << endl;
+
+    return 0;
+}
