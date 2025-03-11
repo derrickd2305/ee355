6d32ece01bef46e1b4aa337c0449df00ec06397a
derrickd2305 <derrickd@usc.edu>
2025-03-10 22:58:36 -0700

    m

--- hw/hw5/part2-2.cpp
+++ hw/hw5/part2-2.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Fibonacci{
+    public:
+        Fibonacci(int n){
+            length = n;
+        }
+        int length;
+        int arr[100];
+
+        void calculate(){
+            arr[0] = 0;
+            arr[1] = 1;
+            for(int i = 2; i < length; i++){
+                arr[i] = arr[i-1] + arr[i-2];
+            }
+        }
+
+        void display(){
+            for(int i = 0; i < length; i++){
+                cout << arr[i] <<", ";
+            }
+            cout << endl;
+        }
+};
+
+int main(){
+    Fibonacci x(10);
+    x.calculate();
+    x.display();
+    return 0;
+}
 
