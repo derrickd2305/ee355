c62a668f04dac91b3b26ea52e1be55485d4422ed
derrickd2305 <derrickd@usc.edu>
2025-03-29 16:14:57 -0700

    m

--- lab9/part3.cpp
+++ lab9/part3.cpp
@@ -17,25 +17,33 @@ struct Node {
 class CircularQueue {
 private:
    Node* tail;
+   int count;
 
 public:
     // Constructor initializes an empty queue
     CircularQueue(){
         tail = NULL;
+        count = 0;
     }
 
     // Destructor to free allocated memory
     ~CircularQueue() {
-        while(tail != NULL){
-            Node* temp = tail;
-            tail = tail->next;
-            delete temp;
+        if(tail == NULL){
+            return;
         }
+        Node* temp1 = tail->next;
+        while(temp1 != tail){
+            Node* temp2 = temp1->next;
+            delete temp1;
+            temp1 = temp2;
+        }
+        delete tail;
+        tail == NULL;
     }
 
     // Function to check if the queue is empty
     bool isEmpty() {
-        if(tail == NULL){
+        if(count == 0){
             return true;
         }
         return false;
@@ -43,34 +51,77 @@ public:
 
     // Function to check if the queue is full
     bool isFull() {
+        if(count >= 8){
+            return true;
+        }
+        return false;
     }
 
     // Function to add an element to the end of the queue (enqueue)
     void enqueue(int data) {
-        // If the queue is full
-        // cout << "Queue is full. Cannot enqueue " << data << "." << endl;
+        // if full 
+        if(isFull()){
+            cout << "Queue is full. Cannot enqueue " << data << "." << endl;
+            return;
+        }
+        
+        // if not full
+        Node* newData = new Node(data);
+
+        // if new term is first term
+        if(tail == NULL){
+            tail = newData;
+            tail->next = tail;
+        }
+        else{
+            newData->next = tail->next;
+            tail = newData;
+        }
+
+        count++;
         
         // Output the information about the enqueued element
-        // cout << "Enqueued: " << data << " (size: " << count << ")" << endl;
+        cout << "Enqueued: " << data << " (size: " << count << ")" << endl;
     }
 
     // Function to remove an element from the front of the queue (dequeue)
-    int dequeue() {
+    void dequeue() {
         // If the queue is empty
-        // cout << "Queue is empty. Cannot dequeue." << endl;
+        if(isEmpty()){
+            cout << "Queue is empty. Cannot dequeue." << endl;
+        }
         
+        // if not empty
+        Node* temp = tail->next;
+        tail->next = temp->next;
+        delete temp;
+        count--;
+
         // Output the information about the dequeued element
-        cout << "Dequeued: " << value << " (size: " << count << ")" << endl;
+        cout << "Dequeued: " << val << " (size: " << count << ")" << endl;
     }
 
     // Function to get the front element of the queue (peek)
     int peek() {
         // If the queue is empty
-        // cout << "Queue is empty." << endl;
+        if(isEmpty()){
+            cout << "Queue is empty." << endl;
+            return -1;
+        }
+
+        // if queue is not empty
+        return tail->val;
     }
 
     // Function to display the queue elements
     void display() {
+        Node* temp = tail->next;
+        cout << "Circular Queue: ";
+        while(temp != tail){
+            cout << temp->val << " -> ";
+            temp = temp->next;
+        }
+        cout << tail << " -> (back to head)" << endl;
     }
 };
 
