ffafd156d1d5ee4718d30b678631fdfad3c8e867
derrickd2305 <derrickd@usc.edu>
2025-04-03 13:17:14 -0700

    m

--- hw/hw6/hw6_q1.cpp
+++ hw/hw6/hw6_q1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    cout << "array of size: ";
+    int size;
+    cin >> size;
+
+    int* arr = new int[size];
+
+    for(int i = 0; i < size; i++){
+        cout << "input value: ";
+        cin >> arr[i];
+    }
+    cout << endl;
+
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    delete [] arr;
+    return 0;
+
+}
