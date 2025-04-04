ba994ffed4746bc179a6387dc8ca0886bcbf852b
derrickd2305 <derrickd@usc.edu>
2025-04-03 21:31:55 -0700

    M

--- hw/hw6/hw6_q5.cpp
+++ hw/hw6/hw6_q5.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
+struct Node {
     int val;
     Node* next;
 
-    Node(int data) : val(data), next(NULL){}
+    Node(int data) : val(data), next(NULL) {}
 };
 
-class CLL{
+class CLL {
     private:
         Node* head;
     public:
         CLL() : head(NULL) {}
 
-        void insert(int data){
+        void insert(int data) {
             Node* newData = new Node(data);
 
-            if(head == NULL){
+            if (head == NULL) {
                 head = newData;
                 head->next = head;
                 return;
             }
 
             Node* temp = head;
-            while(temp->next != head){
+            while (temp->next != head) {
                 temp = temp->next;
             }
 
@@ -32,72 +32,87 @@ class CLL{
             newData->next = head;
         }
 
-    void remove(int data){
-        if(head == NULL){
-            cout << "no elements to remove" << endl;
-            return;
-        }
-
-        // case: head is target and is only term
-        if(head->next == head){
-            if(head->val == data){
-                delete head;
-                head = NULL;
+        void remove(int data) {
+            if (head == NULL) {
+                cout << "No elements to remove" << endl;
+                return;
             }
-            return;
-        }
 
-        Node* curr = head;
-        Node* prev = NULL;
+            Node* curr = head;
+            Node* prev = NULL;
 
-        while(curr->next != head){
-            if(curr->val = data){
-                prev = head;
-                while(prev->next != curr){
-                    prev = prev->next;
+            // Case: Head is the target
+            if (head->val == data) {
+                if (head->next == head) { // Only one element in the list
+                    delete head;
+                    head = NULL;
+                    return;
                 }
-                prev->next = curr->next;
+
+                // Find the last node
+                Node* last = head;
+                while (last->next != head) {
+                    last = last->next;
+                }
+
+                head = head->next; // Move head forward
+                last->next = head; // Update last node's next
                 delete curr;
                 return;
             }
-            curr = curr->next;
+
+            // Case: Non-head nodes
+            while (curr->next != head) {
+                if (curr->val == data) {
+                    prev->next = curr->next;
+                    delete curr;
+                    return;
+                }
+                prev = curr;
+                curr = curr->next;
+            }
+
+            // If last node is the target
+            if (curr->val == data) {
+                prev->next = head;
+                delete curr;
+                return;
+            }
+
+            cout << "Value not found" << endl;
         }
-        cout << "value not found" << endl;
 
-    }
+        bool search(int data) {
+            if (head == NULL) {
+                return false;
+            }
 
-    bool search(int data){
-        if(head == NULL){
+            Node* temp = head;
+
+            do {
+                if (temp->val == data) {
+                    return true;
+                }
+                temp = temp->next;
+            } while (temp != head);
+
             return false;
         }
 
-        Node* temp = head;
-
-        do{
-            if(temp->val == data){
-                return true;
+        void printList() {
+            if (head == NULL) {
+                cout << "List empty\n";
+                return;
             }
-            temp = temp->next;
-        }while(temp != head);
 
-        return false;
-    }
+            Node* temp = head;
 
-    void printList(){
-        if(head == NULL){
-            cout << "list empty\n";
-            return;
+            do {
+                cout << temp->val << " ";
+                temp = temp->next;
+            } while (temp != head);
+            cout << endl;
         }
-
-        Node* temp = head;
-
-        do{
-            cout << temp->val << " ";
-            temp = temp->next;
-        }while(temp != head);
-        cout << endl;
-    }
-
 };
 
 int main() {
@@ -107,7 +122,7 @@ int main() {
     cll.insert(20);
     cll.insert(30);
     cll.insert(40);
-    cll.printList(); 
+    cll.printList();
 
     cout << "Search 30: " << (cll.search(30) ? "Found" : "Not Found") << endl;
     cout << "Search 50: " << (cll.search(50) ? "Found" : "Not Found") << endl;
@@ -116,9 +131,9 @@ int main() {
     cll.printList();
 
     cll.remove(30); // Removing middle
-    cll.printList(); 
+    cll.printList();
 
-    cll.remove(50); 
+    cll.remove(50); // Attempting to remove non-existent element
     cll.printList();
 
     return 0;
