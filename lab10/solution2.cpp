fbad6672288015d946f1d128180d32e0b6292d99
derrickd2305 <derrickd@usc.edu>
2025-04-23 15:44:46 -0700

    Create solution2.cpp

---
+++ lab10/solution2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+
+const int NUM_PHILOSOPHERS = 5;
+const int NUM_CYCLES = 10;
+
+std::mutex chopsticks[NUM_PHILOSOPHERS];
+
+// This will cause deadlock
+void philosopher_naive(int id) {
+    int left = id;
+    int right = (id + 1) % NUM_PHILOSOPHERS;
+
+    for (int i = 0; i < NUM_CYCLES; ++i) {
+        std::cout << "[P" << id << "] Thinking...\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(100 + id * 10));
+
+        std::cout << "[P" << id << "] Hungry, trying to pick up left " << left << " and right " << right << "\n";
+
+        chopsticks[left].lock();
+        std::cout << "[P" << id << "] Got left chopstick " << left << "\n";
+        
+        // Add a delay between picking up chopsticks to increase deadlock chance
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        
+        chopsticks[right].lock();
+        std::cout << "[P" << id << "] Got right chopstick " << right << "\n";
+
+        std::cout << "[P" << id << "] Eating...\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        chopsticks[right].unlock();
+        chopsticks[left].unlock();
+
+        std::cout << "[P" << id << "] Finished eating and put down chopsticks.\n";
+    }
+}
+
+
+int main() {
+    std::vector<std::thread> philosophers;
+
+    std::cout << "Starting Dining Philosophers Simulation (Safe Version)...\n";
+
+    for (int i = 0; i < NUM_PHILOSOPHERS; ++i) {
+        philosophers.push_back(std::thread(philosopher_naive, i)); // Use to test deadlock
+    }
+
+    for (size_t i = 0; i < philosophers.size(); ++i) {
+        // TODO: Wait until all the threads finished. 
+    }
+
+    std::cout << "All philosophers finished their meals. No deadlock detected.\n";
+    return 0;
+}
